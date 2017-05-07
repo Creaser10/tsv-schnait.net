9c4a0f65c7037575ed315dff2ae01f6ddf0777fd
p347659 <p347659@localdomain>
2017-05-07 15:41:21 +0200

    first commit

---
+++ Resources/Public/JavaScript/tsv.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* Custom JS für TSV */
+/*jQuery('.typo3-neos-nodetypes-image a.lightbox, .typo3-neos-nodetypes-textwithimage a.lightbox').fancybox();*/
+
+/* Anker Funktion */
+/*
+function jump(h){
+    var top = document.getElementById(h).offsetTop;
+    window.scrollTo(0, top);
+    
+    var url = location.href;
+    location.href = "#"+h;
+    history.replaceState(null,null,url);
+}
+*/
+
+/* Fancybox */
+jQuery('.tsv-schnait-fancyimage a.fancybox').fancybox();
